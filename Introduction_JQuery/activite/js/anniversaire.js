41e57ca8203d3c2d63c5090689315d9dcf60813b
rasengseb <deathseb@gmail.com>
2020-05-26 19:01:13 +0200

    Chapitre 5 cours js

--- Introduction_JQuery/activite/js/anniversaire.js
+++ Introduction_JQuery/activite/js/anniversaire.js
@@ -1,18 +1,33 @@
 $(function() {
 
-    var $list, $newItemForm;
+    let messages = ["You're a great friend.", "Today' your's birthday.", "Wishing you all the best."]
+    let $list, $newItemForm;
+    let indiceMessage = 0;
+    let classCouleurs = ["alert-success", "alert-warning", "alert-danger"]
     $list = $('ul');
     $newItemForm = $('#newItemForm');
 
     $newItemForm.on('submit', function(e) {
         e.preventDefault();
-        var text = $('input:text').val();
-        $list.append('<li>' + text + '</li>');
-        $('input:text').val('');
+        $list.append('<li>' + messages[$list.length] + '</li>');
     });
 
-    $list.on('click', 'li', function() {
-        var $this = $(this);
+    $list.on('click', function() {
+        if(indiceMessage < 3){
+            $list.append('<li class="'+ classCouleurs[indiceMessage] +'">' + messages[indiceMessage] + '</li>');
+            indiceMessage ++;
+        } else{
+            if(indiceMessage === 3){
+                let bodyStyle = getComputedStyle(document.getElementsByTagName("body"));
+                bodyStyle.backgroundImage = "url(anniversaire.jpg)";
+                for (i = 0; i < list.length; i++){
+                    let newElt = document.createElement("li");
+                    newElt.textContent = "Happy Birthday !!!";
+                    newElt.class = "alert-success";
+                    document.getElementsByClassName(classCouleurs[i]).replaceChild(newElt, document.getElementsByClassName(classCouleurs[i]));
+                }
+            }
+        }
     });
 
 });

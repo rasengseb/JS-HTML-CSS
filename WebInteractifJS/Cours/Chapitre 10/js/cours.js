635423daa9eb783dd9895525487997ac396e4a9d
rasengseb <deathseb@gmail.com>
2020-06-01 17:59:18 +0200

    Dernier chapitre js web

---
+++ WebInteractifJS/Cours/Chapitre 10/js/cours.js
@@ -0,0 +1,37 @@
+// Création d'un objet FormData
+var identite = new FormData();
+// Ajout d'information dans l'objet
+identite.append("login", "Bob");
+identite.append("password", "azerty");
+// Création et configuration d'une requête HTTP POST vers le fichier post_form.php
+var req = new XMLHttpRequest();
+req.open("POST", "http://localhost/javascript-web-srv/post_form.php");
+// Envoi de la requête en y incluant l'objet
+req.send(identite);
+
+
+//==== Uitlisation de la fonction ajaxPost
+var commande = new FormData();
+commande.append("couleur", "rouge");
+commande.append("pointure", "43");
+// Envoi de l'objet FormData au serveur
+ajaxPost("http://localhost/javascript-web-srv/post_form.php", commande,
+    function (reponse) {
+        // Affichage dans la console en cas de succès
+        console.log("Commande envoyée au serveur");
+    }
+);
+
+
+
+//====== ENVOIE SIMPLIFIE D'UN FORMULAIRE
+var form = document.querySelector("form");
+// Gestion de la soumission du formulaire
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Récupération des champs du formulaire dans l'objet FormData
+    var data = new FormData(form);
+    // Envoi des données du formulaire au serveur
+    // La fonction callback est ici vide
+    ajaxPost("http://localhost/javascript-web-srv/post_form.php", data, function () {});
+});
